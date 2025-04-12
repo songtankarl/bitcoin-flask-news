28e774c778e791b6442ed29c435f747e3f0196cc
songtankarl <59520699+songtankarl@users.noreply.github.com>
2025-04-12 16:46:25 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 import requests
 import os
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -8,30 +9,38 @@ API_KEY = os.environ.get('NEWSAPI_KEY') or 'f509959c999b4d7e98cc62d75c0b3102'
 NEWS_ENDPOINT = 'https://newsapi.org/v2/everything'
 
 def fetch_bitcoin_news():
-    params = {
-        'q': '비트코인',
-        'language': 'ko',
-        'sortBy': 'publishedAt',
-        'pageSize': 100,
-        'apiKey': API_KEY
-    }
-    response = requests.get(NEWS_ENDPOINT, params=params)
-    data = response.json()
+    from_date = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d')
 
+    all_articles = []
     seen_sources = set()
-    filtered_articles = []
 
-    for article in data.get("articles", []):
-        source = article["source"]["name"]
-        if source not in seen_sources:
-            seen_sources.add(source)
-            filtered_articles.append({
-                'title': article["title"],
-                'url': article["url"],
-                'source': source
-            })
+    for page in range(1, 6):  # 1~5페이지 반복
+        params = {
+            'q': '비트코인',
+            'language': 'ko',
+            'from': from_date,
+            'sortBy': 'publishedAt',
+            'pageSize': 100,
+            'page': page,
+            'apiKey': API_KEY
+        }
+        response = requests.get(NEWS_ENDPOINT, params=params)
+        data = response.json()
 
-    return filtered_articles
+        for article in data.get("articles", []):
+            source = article["source"]["name"]
+            if source not in seen_sources:
+                seen_sources.add(source)
+                all_articles.append({
+                    'title': article["title"],
+                    'url': article["url"],
+                    'source': source
+                })
+
+        if len(data.get("articles", [])) < 100:
+            break
+
+    return all_articles
 
 @app.route('/')
 def index():
